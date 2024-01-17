7c457ba00e4e61f8856fc3b04cfdd511d2d1cee3
Auberon <alai031@ucr.edu>
2024-01-16 20:52:31 -0800

    Added nav bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,18 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import '@/app/ui/global.css';
+
+import Navbar from '@/app/ui/dashboard/navbar';
+ 
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+          <div className="w-full flex-none md:w-64">
+            <Navbar />
+          </div>
+          <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+        </div>
+      </body>
     </html>
   );
 }
