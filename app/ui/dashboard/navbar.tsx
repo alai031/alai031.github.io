bcb525bf34c22083a3b846dece2c80eeee527f2f
Auberon <alai031@ucr.edu>
2024-01-21 16:56:09 -0800

    Fixed correct routing by navbar for mobile when on projects screen

--- app/ui/dashboard/navbar.tsx
+++ app/ui/dashboard/navbar.tsx
@@ -131,16 +131,16 @@ export default function NavBar() {
             <Link href='/#home'>
               <li onClick={()=> setNav(false)} className='py-4 text-sm'>Home</li>
             </Link>
-            <Link href='#about'>
+            <Link href='/#about'>
               <li onClick={()=> setNav(false)} className='py-4 text-sm'>About</li>
             </Link>
-            <Link href='#skills'>
+            <Link href='/#skills'>
               <li onClick={()=> setNav(false)} className='py-4 text-sm'>Skills</li>
             </Link>
-            <Link href='#projects'>
+            <Link href='/#projects'>
               <li onClick={()=> setNav(false)} className='py-4 text-sm'>Projects</li>
             </Link>
-            <Link href='#contact'>
+            <Link href='/#contact'>
               <li onClick={()=> setNav(false)} className='py-4 text-sm'>Contact</li>
             </Link>
           </ul>
